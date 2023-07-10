2d1e700eeb9bca4b9bf26919d1327ee88d945dc5
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-07-10 13:50:33 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+    first_num = 0
+    sec_num = 1
+    num = 0
+    
+    for i in range(length):
+        if i in {0,1}:
+         my_list.append(i)
+         
+        else:
+         my_list.append(first_num + sec_num)
+         num = sec_num
+         sec_num = first_num + sec_num
+         first_num = num
+
+    print(my_list)
+
+print_fibonacci(0)
